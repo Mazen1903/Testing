d83c0f8d5f0979e44fce452677a1d78b44798ad3
Mazen1903 <mazincoman@gmail.com>
2025-08-24 09:43:33 -0500

    Updated supplications.tsx

--- src/components/supplications/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as SupplicationsScreen } from './SupplicationsScreen';
-export { CategoryList } from './CategoryList';
-export { SupplicationsList } from './SupplicationsList';
-export { RemindersTab } from './RemindersTab';
-export { ReminderModal } from './ReminderModal';
-export { CustomTimePicker } from './CustomTimePicker';
